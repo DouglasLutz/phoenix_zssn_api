2476805ef2c24cd8e27115183a4c5749dad31640
Douglas Lutz <douglasdiehllutz@hotmail.com>
2020-06-13 17:30:08 -0300

    Add item seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,4 +10,10 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias zssn.Repo
+alias Zssn.Repo
+alias Zssn.Resources.Item
+
+Repo.insert! %Item{name: "Water", value: 4}
+Repo.insert! %Item{name: "Food", value: 3}
+Repo.insert! %Item{name: "Medication", value: 2}
+Repo.insert! %Item{name: "Ammunition", value: 1}
