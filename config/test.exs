452f026b7169d9073df2349b95a5f37e6ebfb82b
Douglas Lutz <douglasdiehllutz@hotmail.com>
2020-06-18 20:33:26 -0300

    Change test config

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :zssn, Zssn.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "projects",
+  password: "projects",
   database: "zssn_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
