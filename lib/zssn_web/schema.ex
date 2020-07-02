ee4f4d4ff5117338a93065fb2cf586aa0006ad33
Douglas Lutz <douglasdiehllutz@hotmail.com>
2020-07-02 15:37:56 -0300

    Move survivor types to it's own module

--- lib/zssn_web/schema.ex
+++ lib/zssn_web/schema.ex
@@ -2,6 +2,9 @@ defmodule ZssnWeb.Schema do
   use Absinthe.Schema
 
   alias ZssnWeb.Resolvers
+
+  import_types __MODULE__.SurvivorTypes
+
   query do
     @desc "The list of survivors in the application"
     field :survivors, list_of(:survivor) do
@@ -9,43 +12,7 @@ defmodule ZssnWeb.Schema do
       arg :order, type: :sort_order, default_value: :asc
       resolve &Resolvers.Resources.survivors/3
     end
-  end
 
-  @desc "A survivor in this big world"
-  object :survivor do
-    field :id, :id
-    field :name, :string
-    field :age, :integer
-    field :gender, :gender
-    field :latitude, :string
-    field :longitude, :string
-    field :reports, :integer
-    field :infected, :boolean
-    field :inserted_at, :date
-  end
-
-  @desc "Filtering options for the survivors list"
-  input_object :survivor_filter do
-    @desc "Matching a name"
-    field :name, :string
-
-    @desc "Matching a gender"
-    field :gender, :gender
-
-    @desc "Infected survivors"
-    field :infected, :boolean
-
-    @desc "Aged above a value"
-    field :aged_above, :integer
-
-    @desc "Aged below a value"
-    field :aged_below, :integer
-
-    @desc "Inserted after this date"
-    field :inserted_after, :date
-
-    @desc "Inserted before this date"
-    field :inserted_before, :date
   end
 
   @desc "Date data structure\nAccepts strings formated as \"YYYY-DD-MM\""
@@ -62,12 +29,6 @@ defmodule ZssnWeb.Schema do
     serialize fn date ->
       Date.to_iso8601(date)
     end
-  end
-
-  @desc "Available options for gender field"
-  enum :gender do
-    value :male, as: "male"
-    value :female, as: "female"
   end
 
   enum :sort_order do
