70b7941a59f7efcf91fb5c7c48637caa79a992a2
Douglas Lutz <douglasdiehllutz@hotmail.com>
2020-07-06 13:27:58 -0300

    Change error format for nested changesets

--- lib/zssn_web/resolvers/support/error_format.ex
+++ lib/zssn_web/resolvers/support/error_format.ex
@@ -2,8 +2,12 @@ defmodule ZssnWeb.Resolvers.Support.ErrorFormat do
   def transform_errors(changeset) do
     changeset
     |> Ecto.Changeset.traverse_errors(&format_error/1)
-    |> Enum.map(fn {key, value} ->
-      %{key: key, message: value}
+    |> Enum.map(fn
+        {key, [%{} = value]} ->
+          %{key: "#{key}.#{value |> Map.keys |> List.first}",
+            message: value |> Map.values |> List.first}
+        {key, value} ->
+          %{key: key, message: value}
     end)
   end
 
